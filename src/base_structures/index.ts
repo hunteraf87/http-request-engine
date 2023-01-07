dfefcb02b45ca6fe07461cb212aa431c5862d0a7
Hunteraf <hunteraf@mail.ru>
2023-01-07 21:16:52 +0300

    Удаление неиспользуемых структур данных

--- src/base_structures/index.ts
+++ src/base_structures/index.ts
@@ -1,5 +1,3 @@
 export {LinkedList} from './linked-list';
 export {SimpleQueue, DoubleQueue} from './queue';
 export {LimitedStack} from './stack';
-export {binarySearch} from './binary-search';
-export {BinaryTree} from './binary-tree';
